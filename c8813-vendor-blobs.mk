4908ff40e139f23efb7e324291ff1af89f36b3a6
zerojim <zerojim520@gmail.com>
2013-05-15 00:12:48 +0800

    Update news file

--- c8813-vendor-blobs.mk
+++ c8813-vendor-blobs.mk
@@ -20,7 +20,6 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/c8813/proprietary/bin/akmd8975:system/bin/akmd8975 \
     vendor/huawei/c8813/proprietary/bin/bridgemgrd:system/bin/bridgemgrd \
     vendor/huawei/c8813/proprietary/bin/gps_check:system/bin/gps_check \
-    vendor/huawei/c8813/proprietary/bin/gpu_dcvsd:system/bin/gpu_dcvsd \
     vendor/huawei/c8813/proprietary/bin/load_oemlogo:system/bin/load_oemlogo \
     vendor/huawei/c8813/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/huawei/c8813/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
@@ -34,6 +33,8 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/c8813/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/huawei/c8813/proprietary/bin/thermald:system/bin/thermald \
     vendor/huawei/c8813/proprietary/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app \
+	vendor/huawei/c8813/proprietary/bin/hciattach:system/bin/hciattach \
+	vendor/huawei/c8813/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
     vendor/huawei/c8813/proprietary/lib/libauth.so:system/lib/libauth.so \
     vendor/huawei/c8813/proprietary/lib/libchromatix_ov5647_default_video.so:system/lib/libchromatix_ov5647_default_video.so \
     vendor/huawei/c8813/proprietary/lib/libchromatix_ov5647_ff_default_video.so:system/lib/libchromatix_ov5647_ff_default_video.so \
@@ -148,18 +149,6 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/c8813/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
     vendor/huawei/c8813/proprietary/lib/libcontrast.so:system/lib/libcontrast.so \
     vendor/huawei/c8813/proprietary/lib/libadc.so:system/lib/libadc.so \
-    vendor/huawei/c8813/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so
-
-#PRODUCT_COPY_FILES += \
-#    vendor/huawei/c8813/proprietary/lib/libproduct_bt_check.so:system/lib/libproduct_bt_check.so \
-#    vendor/huawei/c8813/proprietary/lib/libwifidiag.so:system/lib/libwifidiag.so \
-#    vendor/huawei/c8813/proprietary/lib/libwifi_test.so:system/lib/libwifi_test.so \
-#    vendor/huawei/c8813/proprietary/bin/ath_supplicant:system/bin/ath_supplicant \
-#    vendor/huawei/c8813/proprietary/bin/widevinetest_rpc:system/bin/widevinetest_rpc \
-#    vendor/huawei/c8813/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
-#    vendor/huawei/c8813/proprietary/bin/hostapd:system/bin/hostapd \
-#    vendor/huawei/c8813/proprietary/bin/hostapd_cli:system/bin/hostapd_cli \
-#    vendor/huawei/c8813/proprietary/bin/ds_fmc_appd:system/bin/bin/ds_fmc_appd
-
-#    vendor/huawei/c8813/proprietary/bin/rild:system/bin/rild \
-#    vendor/huawei/c8813/proprietary/lib/libril.so:system/lib/libril.so \
+    vendor/huawei/c8813/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
+    vendor/huawei/c8813/proprietary/bin/rild:system/bin/rild \
+    vendor/huawei/c8813/proprietary/lib/libril.so:system/lib/libril.so \
